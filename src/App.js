2b6854ed0885a638af3369f4ae2af5b1e8416135
oyoyoi <marcobustillo21@gmail.com>
2018-10-29 14:52:07 +0800

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            Learn Reacttest
           </a>
         </header>
       </div>
